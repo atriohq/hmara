2befe81d3ab03a5279374b6eda68252af900dce5
Michael Seevogel <git@michaelseevogel.de>
2020-10-22 13:46:32 +0200

    updated upd_dev_collection.sql

--- install/sql/incremental/upd_dev_collection.sql
+++ install/sql/incremental/upd_dev_collection.sql
@@ -1,8 +1,6 @@
 ALTER TABLE `client_template` ADD `limit_access_mail_autoresponder` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'n' AFTER `web_servers`;
 ALTER TABLE `client_template` ADD `limit_access_mail_filter` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'n' AFTER `limit_access_mail_autoresponder`;
-ALTER TABLE `client_template` ADD `limit_access_mail_custom_rules` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'n' AFTER `limit_access_mail_filter`; 
-ALTER TABLE `client_template` ADD `limit_access_mail_backup` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'n' AFTER `limit_access_mail_custom_rules`;
+ALTER TABLE `client_template` ADD `limit_access_mail_backup` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'n' AFTER `limit_access_mail_filter`;
 ALTER TABLE `client` ADD `limit_access_mail_autoresponder` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'y' AFTER `web_servers`;
 ALTER TABLE `client` ADD `limit_access_mail_filter` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'y' AFTER `limit_access_mail_autoresponder`;
-ALTER TABLE `client` ADD `limit_access_mail_custom_rules` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'y' AFTER `limit_access_mail_filter`;
-ALTER TABLE `client` ADD `limit_access_mail_backup` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'y' AFTER `limit_access_mail_custom_rules`;
+ALTER TABLE `client` ADD `limit_access_mail_backup` ENUM('n','y') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'y' AFTER `limit_access_mail_filter`;
