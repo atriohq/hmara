e4a1d6eb91a19e96196bfdceffb939941e3d2699
Herman van Rink <rink@initfour.nl>
2022-04-08 21:18:42 +0200

    Add Graphite export for email quota cronjob.

--- server/lib/classes/cron.d/100-monitor_email_quota.inc.php
+++ server/lib/classes/cron.d/100-monitor_email_quota.inc.php
@@ -152,6 +152,28 @@ class cronjob_monitor_email_quota extends cronjob {
 		/* The new data is written, now we can delete the old one */
 		$this->_tools->delOldRecords($res['type'], $res['server_id']);
 
+		/**
+		 * Export data to Graphite
+		 *
+		 * Install:
+		 * Add to the server/lib/config.inc.local.php file: `$conf['graphite_collector_command'] = 'ssh collector@icinga.local dummy_netcat';`
+		 *
+		 * On the icinga server create a user collector, with in the .ssh/authorized_keys: `command="nc -q0 127.0.0.1 2003" ssh-rsa ...` with the ssh public key of the root user on the mailserver
+		 * The dummy_netcat is replaced by the actual nc command, assuring that no other commands can be executed via this key.
+		 */
+		if (!empty($data) && !empty($conf['graphite_collector_command'])) {
+			$server_config = $app->getconf->get_server_config($conf['server_id'], 'server');
+			$hostname = preg_replace('/\./', '_', $server_config['hostname']);
+
+			$graphite_lines = '';
+			$timestamp = time();
+			foreach ($data as $username => $size) {
+				$username = preg_replace('/\./', '_', $username);
+				$graphite_lines .= "ispconfig.$hostname.monitor_data.email_quota.$username $size[used] $timestamp" . PHP_EOL;
+			}
+			file_put_contents('/tmp/usage.ssv', $graphite_lines);
+			shell_exec("cat /tmp/usage.ssv | " . $conf['graphite_collector_command']);
+		}
 
 		parent::onRunJob();
 	}
