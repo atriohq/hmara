86f21484d2011186e080baa9f8804b3479a7a91f
Michael Seevogel <git@michaelseevogel.de>
2024-01-14 12:07:32 +0100

    remove debug line

--- server/plugins-available/cron_plugin.inc.php
+++ server/plugins-available/cron_plugin.inc.php
@@ -273,7 +273,6 @@ class cron_plugin {
 						$app->log("PHP CLI binary not set for the website\'s selected PHP version or Default was selected. Fall back to \"/usr/bin/php\" for cronjob id " . $job['id'], LOGLEVEL_DEBUG);
 						if($job['type'] == 'chrooted') {
 							if(!file_exists($this->parent_domain['document_root'] . $web_php_cli)) {
-								echo $this->parent_domain['document_root'] . $web_php_cli."\n\n\n\n";
 								$app->log("The PHP cli binary " . $web_php_cli . " is not available in the jail of the web " . $web_domain . " / cronjob_id: " . $job['id']  . ". Check your Jailkit setup!", LOGLEVEL_DEBUG);
 							}
 						}
