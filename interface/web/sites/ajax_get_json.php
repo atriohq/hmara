fd9b8930fb1d723ce960703159007020768e62b0
Michael Seevogel <git@michaelseevogel.de>
2024-01-22 15:17:18 +0100

    Don't check permissions of the web when a user wants to create a new cronjob

--- interface/web/sites/ajax_get_json.php
+++ interface/web/sites/ajax_get_json.php
@@ -253,10 +253,11 @@ if($type == 'getcronplaceholders') {
 	$web_docroot_client = '';
 
 	if($web_id > 0) {
+
 		$web = $app->db->queryOneRecord("SELECT wd.sys_groupid, wd.domain, wd.document_root, sp.php_cli_binary
 		FROM web_domain wd
 		LEFT JOIN server_php sp ON wd.server_php_id = sp.server_php_id
-		WHERE wd.domain_id = ? AND ".$app->tform->getAuthSQL('r'), $web_id);
+		WHERE wd.domain_id = ?", $web_id);
 
 		$php_cli_binary = $web['php_cli_binary'];
 
