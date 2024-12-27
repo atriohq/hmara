9dc65580f8dae6ed9ad71243289246580d2f46b3
Herman van Rink <rink@initfour.nl>
2024-12-27 14:59:03 +0100

    Comments

--- interface/lib/classes/functions.inc.php
+++ interface/lib/classes/functions.inc.php
@@ -214,6 +214,7 @@ class functions {
 			$regex = "/(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/";
 		}
 
+		// Webserver IP's
 		$server_by_id = array();
 		$server_by_ip = array();
 		$servers = $app->db->queryAllRecords("SELECT * FROM server");
@@ -223,6 +224,7 @@ class functions {
 			}
 		}
 
+		// Additional server IP's
 		$localips = array();
 		$ips = array();
 		$results = $app->db->queryAllRecords("SELECT ip_address AS ip, server_id FROM server_ip WHERE ip_type = ?", $type);
@@ -235,6 +237,7 @@ class functions {
 			}
 		}
 
+		// OpenVZ IP's
 		$results = $app->db->queryAllRecords("SELECT ip_address AS ip FROM openvz_ip");
 		if(!empty($results) && is_array($results)){
 			foreach($results as $result){
@@ -256,6 +259,7 @@ class functions {
 			}
 		}
 
+		// DNS slave IP's
 		if ($app->auth->is_admin()) {
 			$results = $app->db->queryAllRecords("SELECT ns AS ip FROM dns_slave");
 		} else {
@@ -269,6 +273,7 @@ class functions {
 			}
 		}
 
+		// IP's used for databases
 		if ($app->auth->is_admin()) {
 			$results = $app->db->queryAllRecords("SELECT database_name as name,remote_ips as ip FROM web_database WHERE remote_ips != ''");
 		} else {
