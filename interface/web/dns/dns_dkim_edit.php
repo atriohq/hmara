7b64546cfad5de467ff007b838beacf0c505b392
Herman van Rink <rink@initfour.nl>
2025-02-28 23:19:57 +0100

    Support external DKIM keys combined with local mail_domain, #5337 attempt 1

--- interface/web/dns/dns_dkim_edit.php
+++ interface/web/dns/dns_dkim_edit.php
@@ -70,23 +70,15 @@ class page_action extends tform_actions {
 		}
 
 		parent::onShowNew();
+	}
 
-        $soa = $app->db->queryOneRecord("SELECT * FROM dns_soa WHERE id = ? AND " . $app->tform->getAuthSQL('r'), $_GET['zone']);
-        $sql=$app->db->queryOneRecord("SELECT domain, dkim_public, dkim_selector, dkim FROM mail_domain WHERE domain = ? AND " . $app->tform->getAuthSQL('r'), substr_replace($soa['origin'],'',-1));
-		if(isset($sql['domain']) && $sql['domain'] != '') {
-			if($sql['dkim'] == 'y') {
-		        $public_key=str_replace(array('-----BEGIN PUBLIC KEY-----','-----END PUBLIC KEY-----',"\r","\n"),'',$sql['dkim_public']);
-				$app->tpl->setVar('public_key', $public_key, true);
-				$app->tpl->setVar('selector', $sql['dkim_selector'], true);
-			} else {
-			//TODO: show warning - use mail_domain for dkim and enabled dkim
-			}
-			$app->tpl->setVar('edit_disabled', 1);
-		} else {
-			$app->tpl->setVar('edit_disabled', 0);
-		}
-		$app->tpl->setVar('name', $soa['origin'], true);
+	function onShowEnd() {
+		global $app, $conf;
 
+		$app->tpl->setVar("selector", str_replace('._domainkey', '', $this->dataRecord['name']), true);
+		$app->tpl->setVar("public_key", str_replace('v=DKIM1; t=s; p=', '', $this->dataRecord['data']), true);
+
+		parent::onShowEnd();
 	}
 
 	function onSubmit() {
@@ -117,7 +109,7 @@ class page_action extends tform_actions {
 		// add dkim-settings to the public-key in the txt-record
 		if (!empty($this->dataRecord['data'])) {
 			$this->dataRecord['data']='v=DKIM1; t=s; p='.$this->dataRecord['data'];
-			$this->dataRecord['name']=$this->dataRecord['selector'].'._domainkey.'.$this->dataRecord['name'];
+			$this->dataRecord['name']=$this->dataRecord['selector'].'._domainkey';
 //			$this->dataRecord['ttl']=60;
 		}
 		// Update the serial number  and timestamp of the RR record
