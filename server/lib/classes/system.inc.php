f64d892fec1f24336460d85a70ae196670fed7b7
Till Brehm <tbrehm@ispconfig.org>
2025-03-09 10:56:51 +0000

    Fixes #1988 better init script failure log message.

--- server/lib/classes/system.inc.php
+++ server/lib/classes/system.inc.php
@@ -2161,13 +2161,13 @@ class system{
     }
 
 		if($full_init_script_path == '') {
-			$app->log('No init script, we quit here.',LOGLEVEL_WARN);
+			$app->log('No init script for '.$servicename.', we quit here.',LOGLEVEL_WARN);
 			return false;
 		}
 
 		//* Check init script
 		if(!is_file($full_init_script_path)) {
-			$app->log('Init script '.$full_init_script_path.' not found',LOGLEVEL_WARN);
+			$app->log('Init script '.$full_init_script_path.' for '.$servicename.' not found',LOGLEVEL_WARN);
 			return false;
 		}
 
