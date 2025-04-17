57636ead1c8ff8f5eb22bbde902851c25f6ec86f
Herman van Rink <rink@initfour.nl>
2025-04-17 16:24:31 +0200

    Avoid php warning

--- server/lib/classes/cron.d/100-monitor_domain_mx.inc.php
+++ server/lib/classes/cron.d/100-monitor_domain_mx.inc.php
@@ -67,7 +67,7 @@ class cronjob_monitor_domain_mx  extends cronjob {
 		$app->uses('getconf,functions');
 		$mail_config = $app->getconf->get_server_config($conf['server_id'], 'mail');
 
-		if ($mail_config['monitor_mx_records'] != 'y') {
+		if (!isset($mail_config['monitor_mx_records']) || $mail_config['monitor_mx_records'] != 'y') {
 			return;
 		}
 		/* used for all monitor cronjobs */
