ca725febbed2919df5ca9e33498b6e9a51b4203e
Herman van Rink <rink@initfour.nl>
2025-07-04 17:12:19 +0200

    Update the user-agent used in cronjob GET requests to be descriptive
    Issue #4020 needed something else then the default from wget, I assume because some system there did not like wget accessing the site.
    However such an old Firefox string is just misleading.

--- server/plugins-available/cron_plugin.inc.php
+++ server/plugins-available/cron_plugin.inc.php
@@ -276,7 +276,7 @@ class cron_plugin {
 						continue;
 					}
 
-					$cron_line .= "\t{$cron_config['wget']} --no-check-certificate --user-agent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0' -q -t 1 -T 7200 -O " . $log_wget_target . " " . escapeshellarg($job['command']) . " " . $log_target;
+					$cron_line .= "\t{$cron_config['wget']} --no-check-certificate --user-agent='ISPConfig managed cronjob' -q -t 1 -T 7200 -O " . $log_wget_target . " " . escapeshellarg($job['command']) . " " . $log_target;
 
 				} else {
 
