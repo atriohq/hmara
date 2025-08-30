b505ab7e7654a6a4e7b9bd41fecac85ac5d29717
Herman van Rink <rink@initfour.nl>
2025-08-30 10:52:08 +0200

    Draft Dovecot 2.4 config for Debian 13, #6920

--- install/lib/install.lib.php
+++ install/lib/install.lib.php
@@ -263,6 +263,13 @@ function get_distname() {
 			$distid = 'debian60';
 			$distbaseid = 'debian';
 			swriteln("Operating System: Debian 12.0 (Bookworm) or compatible\n");
+		} elseif(substr(trim(file_get_contents('/etc/debian_version')),0,2) == '13') {
+			$distname = 'Debian';
+			$distver = 'Trixie';
+			$distconfid = 'debian130';
+			$distid = 'debian60';
+			$distbaseid = 'debian';
+			swriteln("Operating System: Debian 13.0 (Trixie) or compatible\n");
 		} elseif(strstr(trim(file_get_contents('/etc/debian_version')), '/sid')) {
 			$distname = 'Debian';
 			$distver = 'Testing';
