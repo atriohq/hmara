51018411b360017e61eaa6212fb357ee5939f732
Herman van Rink <rink@initfour.nl>
2025-10-16 19:41:56 +0200

    add hint

--- interface/web/sites/stats_proxy/index.php
+++ interface/web/sites/stats_proxy/index.php
@@ -107,6 +107,7 @@ if ($passthroughdata === false) {
     if ($status_code != 200) {
         header("HTTP/1.1 $status_code");
         echo "Error: Sorry the backend site returned HTTP status code $status_code";
+        // This could mean that the site has not been updated yet, to store the passwordt in web/stats/.htpasswd_stats
         die();
     }
 
