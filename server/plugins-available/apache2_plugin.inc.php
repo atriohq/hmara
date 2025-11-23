8f113ad81f97f31c7309b32d2d11bc8e73d1f6e1
Herman van Rink <rink@initfour.nl>
2025-11-23 22:17:26 +0100

    pre set permissions on log dir and error.log file

--- server/plugins-available/apache2_plugin.inc.php
+++ server/plugins-available/apache2_plugin.inc.php
@@ -834,10 +834,12 @@ class apache2_plugin {
 		if(!is_dir($data['new']['document_root'].'/'.$log_folder) || !is_dir('/var/log/ispconfig/httpd/'.$data['new']['domain']) || is_link($data['new']['document_root'].'/'.$log_folder)) {
 			if(is_link($data['new']['document_root'].'/'.$log_folder)) unlink($data['new']['document_root'].'/'.$log_folder);
 			if(!is_dir('/var/log/ispconfig/httpd/'.$data['new']['domain'])) $app->system->exec_safe('mkdir -p ?', '/var/log/ispconfig/httpd/'.$data['new']['domain']);
+			$app->system->chgrp('/var/log/ispconfig/httpd/'.$data['new']['domain'], $data['new']['system_group'] );
+			$app->system->chmod('/var/log/ispconfig/httpd/'.$data['new']['domain'], 0750);
 			$app->system->mkdirpath($data['new']['document_root'].'/'.$log_folder);
 			$app->system->chown($data['new']['document_root'].'/'.$log_folder, 'root');
-			$app->system->chgrp($data['new']['document_root'].'/'.$log_folder, 'root');
-			$app->system->chmod($data['new']['document_root'].'/'.$log_folder, 0755);
+			$app->system->chgrp($data['new']['document_root'].'/'.$log_folder, $data['new']['system_group'] );
+			$app->system->chmod($data['new']['document_root'].'/'.$log_folder, 0750);
 			$app->system->exec_safe('mount --bind ? ?', '/var/log/ispconfig/httpd/'.$data['new']['domain'], $data['new']['document_root'].'/'.$log_folder);
 			//* add mountpoint to fstab
 			$fstab_line = '/var/log/ispconfig/httpd/'.$data['new']['domain'].' '.$data['new']['document_root'].'/'.$log_folder.'    none    bind,nofail';
@@ -1026,18 +1028,18 @@ class apache2_plugin {
 
 			if($web_config['security_level'] == 20) {
 				$web_folder_permission = (isset($web_config['web_folder_permission']))?octdec($web_config['web_folder_permission']):0711;
-				$app->system->chmod($data['new']['document_root'], 0755);
+				$app->system->chmod($data['new']['document_root'], 0755);  # With a chown to client_group this could be 750
 				$app->system->chmod($data['new']['document_root'].'/web', $web_folder_permission);
 				$app->system->chmod($data['new']['document_root'].'/webdav', 0710);
 				$app->system->chmod($data['new']['document_root'].'/private', 0710);
-				$app->system->chmod($data['new']['document_root'].'/ssl', 0755);
+				$app->system->chmod($data['new']['document_root'].'/ssl', 0755); # With a chown to client_group this could be 750
 
 				// make tmp directory writable for Apache and the website users
 				$app->system->chmod($data['new']['document_root'].'/tmp', 0770);
 
-				// Set Log directory to 755 to make the logs accessible by the FTP user
+				// Set Log directory to 750 to make the logs accessible by the FTP user via the client group
 				if(realpath($data['new']['document_root'].'/'.$log_folder . '/error.log') == '/var/log/ispconfig/httpd/'.$data['new']['domain'].'/error.log') {
-					$app->system->chmod($data['new']['document_root'].'/'.$log_folder, 0755);
+					$app->system->chmod($data['new']['document_root'].'/'.$log_folder, 0750);
 				}
 
 				if($web_config['add_web_users_to_sshusers_group'] == 'y') {
@@ -1156,12 +1158,13 @@ class apache2_plugin {
 		$app->system->web_folder_protection($data['new']['document_root'], true);
 
 		if($data['new']['type'] == 'vhost') {
-			// Change the ownership of the error log to the root user
+			// Change the ownership of the error log to the root user, with the client group.
 			if(!@is_file('/var/log/ispconfig/httpd/'.$data['new']['domain'].'/error.log')) {
 				$app->system->exec_safe('touch ?', '/var/log/ispconfig/httpd/'.$data['new']['domain'].'/error.log');
 			}
 			$app->system->chown('/var/log/ispconfig/httpd/'.$data['new']['domain'].'/error.log', 'root');
-			$app->system->chgrp('/var/log/ispconfig/httpd/'.$data['new']['domain'].'/error.log', 'root');
+			$app->system->chgrp('/var/log/ispconfig/httpd/'.$data['new']['domain'].'/error.log', $groupname);
+			$app->system->chmod('/var/log/ispconfig/httpd/'.$data['new']['domain'].'/error.log', 0640);
 		}
 
 		//* Write the custom php.ini file, if custom_php_ini field is not empty
