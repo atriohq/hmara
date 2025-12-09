e11aadae96b365e90023cdab6d565a0a8136409c
Demian <demian@pyte.dev>
2025-12-09 15:20:28 +0100

    fixed wrong default validation_server value #6912

--- install/sql/incremental/upd_dev_collection.sql
+++ install/sql/incremental/upd_dev_collection.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `mail_relay_recipient` ADD COLUMN `validation_server` VARCHAR(255) DEFAULT 'OK';
+ALTER TABLE `mail_relay_recipient` ADD COLUMN `validation_server` VARCHAR(255) NULL;
 -- Add last_password_change column to sys_user table
 ALTER TABLE `sys_user` ADD COLUMN `last_password_change` DATE NULL DEFAULT CURDATE();
