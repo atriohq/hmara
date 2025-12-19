6935cef698765b49481707a84619ec9350361e57
Till <t.brehm@ispconfig.org>
2025-12-19 19:06:48 +0100

    Fixes #6944 Set useragent for cronjobs (wget)

--- server/plugins-available/cron_plugin.inc.php
+++ server/plugins-available/cron_plugin.inc.php
@@ -276,7 +276,7 @@ class cron_plugin {
 						continue;
 					}
 
-					$cron_line .= "\t{$cron_config['wget']} --no-check-certificate --user-agent='ISPConfig managed cronjob' -q -t 1 -T 7200 -O " . $log_wget_target . " " . escapeshellarg($job['command']) . " " . $log_target;
+					$cron_line .= "\t{$cron_config['wget']} --no-check-certificate --user-agent='Mozilla/5.0 (compatible; ISPConfig/3.0; +https://www.ispconfig.org/)' -q -t 1 -T 7200 -O " . $log_wget_target . " " . escapeshellarg($job['command']) . " " . $log_target;
 
 				} else {
 
