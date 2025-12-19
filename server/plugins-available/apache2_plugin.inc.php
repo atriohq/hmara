40a55ccbc7067cd289e590a3b1955f7d99efdcf7
Till <t.brehm@ispconfig.org>
2025-12-19 18:24:13 +0100

    Fixes #6918 Remove OCSP stapling sections in vhosts since Lets Encrypt no longer supports them.

--- server/plugins-available/apache2_plugin.inc.php
+++ server/plugins-available/apache2_plugin.inc.php
@@ -1808,6 +1808,14 @@ class apache2_plugin {
 
 		unset($tmp_vhost_arr);
 
+		//* Check OCSP support once for all vhosts
+		if($data['new']['ssl_domain'] != '' && $data['new']['ssl'] == 'y' && @is_file($crt_file)) {
+			$ocsp_uri = trim($app->system->exec_safe('openssl x509 -noout -ocsp_uri -in ?', $crt_file));
+			if (!empty($ocsp_uri)) {
+				$tpl->setVar('ssl_ocsp_supported', 'y');
+			}
+		}
+
 		//* Add vhost for ipv4 IP with SSL
 		if($data['new']['ssl_domain'] != '' && $data['new']['ssl'] == 'y' && @is_file($crt_file) && @is_file($key_file) && (@filesize($crt_file)>0)  && (@filesize($key_file)>0)) {
 			$tmp_vhost_arr = array('ip_address' => $data['new']['ip_address'], 'ssl_enabled' => 1, 'port' => '443');
@@ -1830,11 +1838,6 @@ class apache2_plugin {
 
 			unset($tmp_vhost_arr, $ipv4_ssl_alias_seo_redirects);
 			$app->log('Enable SSL for: '.$domain, LOGLEVEL_DEBUG);
-
-			$ocsp_uri = $app->system->exec_safe('openssl x509 -noout -ocsp_uri -in ?', $vhost_data['ssl_crt_file']);
-			if (!empty($ocsp_uri) && $data['new']['ssl'] == 'y') {
-				$tpl->setVar('ssl_ocsp_supported', 'y');
-			}
 		}
 
 		//* Add vhost for IPv6 IP
